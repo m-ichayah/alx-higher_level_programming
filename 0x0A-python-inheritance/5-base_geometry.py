695a15c2242ac4a1a9982cde7177059d96441656
m-ichayah <ini.michayah@gmail.com>
2023-01-09 14:48:01 +0000

    Python Inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Iniovosa Michayah O
+"""
+an empty class definition
+"""
+
+
+class BaseGeometry:
+    """
+    an empty class BaseGeometry
+    """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+# Iniovosa Michayah O
+"""
+a definition of a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """A class definition"""
+
+    def area(self):
+        """
+        raises an Exception with the message
+        area() is not implemented
+        """
+        raise Exception("area() is not implemented")
