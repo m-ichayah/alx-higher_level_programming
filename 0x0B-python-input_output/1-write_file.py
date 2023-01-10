aa3184a6912911b221e06cf179e6a02797d7c65b
m-ichayah <ini.michayah@gmail.com>
2023-01-10 20:27:40 +0000

    function that writes a string to a text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 1-number_of_lines.py
+# Iniovosa Michayah O
+"""
+a function that writes a string to a
+text file (UTF8) and returns the number
+of characters
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    write_file function
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        write_txt = f.write(text)
+        return write_txt
