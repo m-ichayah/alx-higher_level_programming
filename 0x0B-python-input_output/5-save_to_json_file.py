f2c9e85cffa11265d383b3922c8af5ab830eeff8
m-ichayah <ini.michayah@gmail.com>
2023-01-10 21:28:44 +0000

    function that writes an Object to a text file using a JSON representation

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 5-to_json_string.py
+# Iniovosa Michayah O
+
+"""
+a function that writes an Object
+to a text file, using a JSON
+representation
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    JSON representation of writing an object
+    """
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
