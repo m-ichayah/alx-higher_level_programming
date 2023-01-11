321c5f495ac8d52642e7bc23d29e8c2f1506a5d5
m-ichayah <ini.michayah@gmail.com>
2023-01-11 00:03:52 +0000

    script that reads stdin line by line and computes metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,63 +1,60 @@
 #!/usr/bin/python3
-"""module for use in log debugging
+# 101-stats.py
+# Iniovosa Michayah O
+"""Reads from standard input and computes metrics.
+
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
 
 
-import sys
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
 
-
-class DebugLog:
-    """class for use in debugging stdin logs
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
     """
-    def __init__(self):
-        self.lines = []
-        self.total_size = 0
-        self.codes_200 = 0
-        self.codes_401 = 0
-        self.codes_403 = 0
-        self.codes_404 = 0
-        self.codes_405 = 0
-        self.codes_500 = 0
-        self.__current_line = 0
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
-    def __str__(self):
-        string = "File size: " + str(self.total_size) + '\n'
-        string += "200: " + str(self.codes_200) + '\n'
-        string += "401: " + str(self.codes_401) + '\n'
-        string += "403: " + str(self.codes_403) + '\n'
-        string += "404: " + str(self.codes_404) + '\n'
-        string += "405: " + str(self.codes_405) + '\n'
-        string += "500: " + str(self.codes_500)
-        return string
+if __name__ == "__main__":
+    import sys
 
-    def update(self, iteration_n):
-        """updates current response numbers
-            with new set of lines
-        """
-        for i in range(self.__current_line, self.__current_line + iteration_n):
-            if "200" in self.lines[i]:
-                self.codes_200 += 1
-            elif "401" in self.lines[i]:
-                self.codes_401 += 1
-            elif "403" in self.lines[i]:
-                self.codes_403 += 1
-            elif "404" in self.lines[i]:
-                self.codes_404 += 1
-            elif "405" in self.lines[i]:
-                self.codes_405 += 1
-            elif "500" in self.lines[i]:
-                self.codes_500 += 1
-            words = line.split(' ')
-            self.total_size += int(words[-1])
-            self.__current_line += 1
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
 
-log = DebugLog()
-lines = []
-line = sys.stdin.readline()
-while line != "":
-    log.lines.append(line)
-    if len(log.lines) % 10 == 0:
-        log.update(10)
-        print(log)
-    line = sys.stdin.readline()
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
