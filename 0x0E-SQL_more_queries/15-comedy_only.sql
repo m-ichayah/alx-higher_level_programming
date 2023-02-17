219ed35ab8f9302b99db4e28c9cabcdd2a45a338
m-ichayah <ini.michayah@gmail.com>
2023-02-17 17:00:14 +0000

    script that lists all Comedy shows in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -5,7 +5,7 @@
 -- You can use only one SELECT statement
 SELECT tv_shows.title -- Query to join tables
 FROM tv_shows
-     JOIN tv_show_genres
+     INNER JOIN tv_show_genres
      	  ON tv_show_genres.show_id = tv_shows.id
      JOIN tv_genres
      	  ON tv_genres.id = tv_show_genres.genre_id
