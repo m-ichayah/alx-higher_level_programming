2472f065e7b4c3014bf976b5de78c37ad7a49f97
m-ichayah <ini.michayah@gmail.com>
2023-03-13 22:05:39 +0000

    script that searches the second biggest integer in the list of arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,29 +1,8 @@
-#!/usr/bin/node
+const args = process.argv.slice(2);
 
-function max (array) {
-  let MaxInt = 0;
-  for (const i of array) {
-    if (i > MaxInt) {
-      MaxInt = i;
-    }
-  }
-  return (MaxInt);
-}
-
-const myarray = [];
-if (process.argv.length <= 3) {
+if (args.length < 2) {
   console.log(0);
 } else {
-  for (let i = 2; i < process.argv.length; i++) {
-    myarray.push(Number(process.argv[i]));
-  }
+  const sortedArgs = args.map(Number).sort((a, b) => b - a);
+  console.log(sortedArgs[1]);
 }
-const MaxInt = max(myarray);
-for (let i = 0; myarray[i] != null; i++) {
-  if (myarray[i] === MaxInt) {
-    delete myarray[i];
-    break;
-  }
-}
-const MaxInt2 = max(myarray);
-console.log(MaxInt2);
