5178c437d1f3d16d978b3bdcacda7530937713c4
m-ichayah <ini.michayah@gmail.com>
2023-03-13 21:58:43 +0000

    script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,17 +1,7 @@
 #!/usr/bin/node
 
-function add(a, b) {
-  return a + b;
+function add (a, b) {
+  return (Number(a) + Number(b));
 }
 
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-
-if (/^-?\d+$/.test(arg1) && /^-?\d+$/.test(arg2)) {
-  const num1 = parseInt(arg1);
-  const num2 = parseInt(arg2);
-
-  console.log(add(num1, num2));
-} else {
-  console.log('Invalid input');
-}
+console.log(add(process.argv[2], process.argv[3]));
