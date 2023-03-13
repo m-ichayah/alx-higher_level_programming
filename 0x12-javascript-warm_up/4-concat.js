6c71e6739183b6c940f364128c7bb4ad558bba10
m-ichayah <ini.michayah@gmail.com>
2023-03-13 21:23:19 +0000

    script that prints two arguments passed to it, in the following format: “ is ”

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,3 @@
 #!/usr/bin/env node
 
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-
-if (arg1 === undefined || arg2 === undefined) {
-  console.log('Error: Two arguments required');
-} else {
-  console.log(`${arg1} is ${arg2}`);
-}
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
